38ab550b295f376b677580a589a5eb4c95e72295
Ajay-vishal3 <71023584+Ajay-vishal3@users.noreply.github.com>
2023-07-14 23:55:48 +0530

    code.py

--- code.py
+++ code.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 from tensorflow import keras
 
-# Load the dataset
+
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
-# Normalize pixel values between 0 and 1
+
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
 
-# Define the model architecture
+
 model = keras.Sequential([
     keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
     keras.layers.MaxPooling2D((2, 2)),
@@ -20,12 +20,12 @@ model = keras.Sequential([
     keras.layers.Dense(10, activation='softmax')
 ])
 
-# Compile the model
+
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-# Train the model
+
 model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
 
 # Evaluate the model
